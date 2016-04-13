9112818e4954f5ea920cc6d3f35b03ded27a66d7
Tiaan Theunissen <tiaant@saiba.org.za>
2016-04-13 20:19:12 +0200

    updated css

--- resources/views/frontend/bookings/index.blade.php
+++ resources/views/frontend/bookings/index.blade.php
@@ -13,7 +13,7 @@
             <div class="row">
                 <div class="divider20"></div>
                 <div class="col-sm-12 col-xs-12 col-md-12 text-center">
-                    <h6 style="color: #ef5505;" class="margin-bottom-0"><small>Early bird prices expire on {{date_format(Carbon\Carbon::parse(App\Models\PricePlan::first()->early_bird_expiry), 'd F Y')}}</small>
+                    <h6 style="color: #00A297;" class="margin-bottom-0"><small style="color: #00A297">Early bird prices expire on {{date_format(Carbon\Carbon::parse(App\Models\PricePlan::first()->early_bird_expiry), 'd F Y')}}</small>
                     </h6>
                 </div>
             </div>
