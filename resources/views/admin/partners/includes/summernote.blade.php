22c6e192baa2787543661b708c74a38be51c6297
Tiaan Theunissen <tiaant@saiba.org.za>
2016-04-15 11:56:07 +0200

    updated site

--- resources/views/admin/partners/includes/summernote.blade.php
+++ resources/views/admin/partners/includes/summernote.blade.php
@@ -5,12 +5,6 @@
             minHeight: null,             // set minimum height of editor
             maxHeight: null,             // set maximum height of editor
             focus: true,                  // set focus to editable area after initializing summernote
-            toolbar: [
-                // [groupName, [list of button]]
-                ['style', ['bold', 'italic', 'underline']],
-                ['font', ['strikethrough']],
-                ['para', ['ul', 'ol', 'paragraph']],
-            ]
         });
     });
 </script>
